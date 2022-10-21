19626e331b520d29827a72cc7fbbd2099ea3e858
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2022-10-21 15:58:51 +0300

    use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,105 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Section from './Section';
 import { Box } from './Box';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   buttonsHandler = option => () =>
+//     this.setState(prevState => ({
+//       ...prevState,
+//       [option]: prevState[option] + 1,
+//     }));
+
+//   countTotalFeedback = () =>
+//     Object.values(this.state).reduce((acc, item) => acc + item, 0);
+
+//   countPositiveFeedbackPercentage = () =>
+//     this.countTotalFeedback()
+//       ? Number(((this.state.good / this.countTotalFeedback()) * 100).toFixed(2))
+//       : 0;
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const options = Object.keys(this.state);
+
+//     return (
+//       <Box display="flex" as="article" p={20}>
+//         <Section title="Please, leave your feedback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.buttonsHandler}
+//           ></FeedbackOptions>
+//         </Section>
+
+//         <Section title="Statistics">
+//           <Statistics
+//             options={options}
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={this.countTotalFeedback()}
+//             positivePercentage={this.countPositiveFeedbackPercentage()}
+//           ></Statistics>
+//         </Section>
+//       </Box>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+  const totalCount = () => good + neutral + bad;
+  const positiveFeedbacks = () =>
+    good / totalCount() ? Number(((good / totalCount()) * 100).toFixed(2)) : 0;
+
+  const buttonsHandler = option => () => {
+    switch (option) {
+      case 'good':
+        setGood(state => state + 1);
+        return;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        return;
+      case 'bad':
+        setBad(state => state + 1);
+        return;
+      default:
+        return;
+    }
   };
 
-  buttonsHandler = option => () =>
-    this.setState(prevState => ({
-      ...prevState,
-      [option]: prevState[option] + 1,
-    }));
+  return (
+    <Box display="flex" as="article" p={20}>
+      <Section title="Please, leave your feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={buttonsHandler}
+        ></FeedbackOptions>
+      </Section>
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, item) => acc + item, 0);
-
-  countPositiveFeedbackPercentage = () =>
-    this.countTotalFeedback()
-      ? Number(((this.state.good / this.countTotalFeedback()) * 100).toFixed(2))
-      : 0;
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-
-    return (
-      <Box display="flex" as="article" p={20}>
-        <Section title="Please, leave your feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.buttonsHandler}
-          ></FeedbackOptions>
-        </Section>
-
-        <Section title="Statistics">
-          <Statistics
-            options={options}
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          ></Statistics>
-        </Section>
-      </Box>
-    );
-  }
-}
+      <Section title="Statistics">
+        <Statistics
+          options={options}
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalCount()}
+          positivePercentage={positiveFeedbacks()}
+        ></Statistics>
+      </Section>
+    </Box>
+  );
+};
 
 export default App;
