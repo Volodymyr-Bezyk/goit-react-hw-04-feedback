a365ab731156fba8af312e45e1b7bab33058f481
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2022-10-24 13:42:12 +0300

    refact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,55 +4,6 @@ import Statistics from './Statistics';
 import Section from './Section';
 import { Box } from './Box';
 
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   buttonsHandler = option => () =>
-//     this.setState(prevState => ({
-//       ...prevState,
-//       [option]: prevState[option] + 1,
-//     }));
-
-//   countTotalFeedback = () =>
-//     Object.values(this.state).reduce((acc, item) => acc + item, 0);
-
-//   countPositiveFeedbackPercentage = () =>
-//     this.countTotalFeedback()
-//       ? Number(((this.state.good / this.countTotalFeedback()) * 100).toFixed(2))
-//       : 0;
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const options = Object.keys(this.state);
-
-//     return (
-//       <Box display="flex" as="article" p={20}>
-//         <Section title="Please, leave your feedback">
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.buttonsHandler}
-//           ></FeedbackOptions>
-//         </Section>
-
-//         <Section title="Statistics">
-//           <Statistics
-//             options={options}
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={this.countTotalFeedback()}
-//             positivePercentage={this.countPositiveFeedbackPercentage()}
-//           ></Statistics>
-//         </Section>
-//       </Box>
-//     );
-//   }
-// }
-
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
